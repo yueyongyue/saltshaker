2a348bf2ad652fc14ffc6e14f014168b94040d67
yueyongyue <yueyognyue@sina.cn>
2015-11-23 13:49:15 +0800

    去掉% 转化成数字型，以便在django 模板里进行比较

--- minions/views.py
+++ minions/views.py
@@ -23,13 +23,12 @@ def minions_hardware_info(request):
     sapi = SaltAPI()
     up_host = sapi.runner_status('status')['up']
     jid = []
-    #disk_dic = {}
     disk_all = {}
     for hostname in up_host:
         info_all = sapi.remote_noarg_execution(hostname,'grains.items')
         disk_use = sapi.remote_noarg_execution(hostname,'disk.usage')
         for key in disk_use:
-            disk_info = {key: disk_use[key]['capacity']}
+            disk_info = {key: int(disk_use[key]['capacity'][:-1])}
             disk_all.update(disk_info)
             disk_dic = {'disk': disk_all}
             info_all.update(disk_dic)
