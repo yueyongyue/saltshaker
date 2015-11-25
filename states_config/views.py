3cb751f5f15dd2e7517db2b068e9502135960cc0
yueyongyue <yueyognyue@sina.cn>
2015-11-25 17:43:39 +0800

    highstate add target

--- states_config/views.py
+++ states_config/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
 from shaker.shaker_core import *
-import os
+from shaker.nodegroups import *
 
 def highstate(request):
+    group = NodeGroups()
+    all = group.list_groups_hosts()
+    return render(request,'states_config/highstate.html',{ 'list_groups': all } )
 
-    return render(request,'states_config/highstate.html', )
+
