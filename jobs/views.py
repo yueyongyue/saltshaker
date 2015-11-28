23a918e7d510b1a4d0e4e0a5123b4d3c8926d643
yueyongyue <yueyognyue@sina.cn>
2015-11-28 15:26:39 +0800

    " "

--- jobs/views.py
+++ jobs/views.py
@@ -5,7 +5,7 @@ import os
 def jobs_history(request):
     sapi = SaltAPI()
     jids = sapi.runner("jobs.list_jobs")
-    return render(request,'jobs/jobs_history.html', { 'jids': jids })
+    return render(request, 'jobs/jobs_history.html', {'jids': jids})
 
 def jobs_manage(request):
     sapi = SaltAPI()
@@ -14,12 +14,12 @@ def jobs_manage(request):
         kill = "salt '*' saltutil.kill_job" + " " + jid
         os.popen(kill)
     jids_running = sapi.runner("jobs.active")
-    return render(request,'jobs/jobs_manage.html',{ 'jids_running': jids_running })
+    return render(request, 'jobs/jobs_manage.html', {'jids_running': jids_running})
 
 def jobs_detail(request,jid):
     jids = "salt-run jobs.lookup_jid" + " " + jid
     detail = os.popen(jids).read()
-    return render(request,'jobs/jobs_detail.html',{ 'detail':detail })
+    return render(request, 'jobs/jobs_detail.html', {'detail': detail})
 
 def jobs_schedule(request):
-    return render(request,'jobs/jobs_schedule.html',)
+    return render(request, 'jobs/jobs_schedule.html')
