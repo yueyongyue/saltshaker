0ae3dadbfb3bc09b858ed510ce63f61b1f172e85
yueyongyue <yueyognyue@sina.cn>
2015-11-29 11:04:38 +0800

    del  login.html url

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -17,6 +17,5 @@ from django.conf.urls import include, url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.index,name='index'),
-    url(r'login.html', views.login,name='login'),
+    url(r'^$', views.index, name='index'),
 ]
