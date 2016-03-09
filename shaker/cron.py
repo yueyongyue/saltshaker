b79ccad105fe8886a2f33777a26d0e6ff3b2c807
yueyongyue <yueyognyue@sina.cn>
2016-03-09 11:09:30 +0800

    修改minion_status 数据获取的api的调用，添加grains 的计划任务

--- shaker/cron.py
+++ shaker/cron.py
@@ -1,12 +1,15 @@
 from shaker.shaker_core import *
 from minions.models import Minions_status
 from shaker.nodegroups import *
+from dashboard.models import *
+from returner.models import *
 
 
 sapi = SaltAPI()
 
 def dashboard_scheduled_job():
-    # minion status
+    '''
+    # minion status save to file
     status_list = []
     status = sapi.runner_status('status')
     key_status = sapi.list_all_key()
@@ -39,36 +42,42 @@ def dashboard_scheduled_job():
         os_all.append(os_dic)
     os_release = list(set(os_list))
 
-    salt_dashboard = file("/tmp/salt_dashboard.tmp", "w+")
+    salt_dashboard = file("/var/cache/salt/master/salt_dashboard.tmp", "w+")
     salt_dashboard.writelines(str(status_list) + '\n')
     salt_dashboard.writelines(str(os_release) + '\n')
     salt_dashboard.writelines(str(os_all) + '\n')
     salt_dashboard.close()
+    '''
+    # minion status save to mysql
+    status = sapi.runner_status('status')
+    key_status = sapi.list_all_key()
+    up = len(status['up'])
+    down = len(status['down'])
+    accepted = len(key_status['minions'])
+    unaccepted = len(key_status['minions_pre'])
+    rejected = len(key_status['minions_rejected'])
+    dashboard_status = Dashboard_status()
+    try:
+        Dashboard_status.objects.get(id=1)
+    except:
+        dashboard_status.id = 1
+        dashboard_status.up = up
+        dashboard_status.down = down
+        dashboard_status.accepted = accepted
+        dashboard_status.unaccepted = unaccepted
+        dashboard_status.rejected = rejected
+        dashboard_status.save()
+    Dashboard_status.objects.filter(id=1).update(id=1, up=up, down=down, accepted=accepted, unaccepted=unaccepted, rejected=rejected)
 
 dashboard = dashboard_scheduled_job()
 
 
 def minions_status_scheduled_job():
-    #status_up = []
-    #status_down = []
-    group = NodeGroups()
-    #sapi = SaltAPI()
     status = Minions_status()
     status_all = sapi.runner_status('status')
     for host_name in status_all['up']:
-        version = sapi.remote_noarg_execution(host_name, 'grains.items')['saltversion']
-        #version = "2015.5.5 (Lithium)"
-        #version_dic = {'version': version}
-        #group_dic = group.hosts_in_group(host_name)
-        #status_dic = {'host': host_name}
-        #status_dic.update(group_dic)
-        #status_dic.update(version_dic)
-        #status_up += [status_dic]
-        #minion_id = Minions_status.objects.get(minion_id=host_name)
-        #minion_id.delete()
-        #status = Minions_status(minion_id=host_name)
-        #update(blog=b)
-        #if minion_id
+        salt_grains = Salt_grains.objects.filter(minion_id=host_name)
+        version = eval(salt_grains[0].grains).get('saltversion').decode('string-escape')
         try:
             Minions_status.objects.get(minion_id=host_name)
         except:
@@ -78,16 +87,8 @@ def minions_status_scheduled_job():
             status.save()
         Minions_status.objects.filter(minion_id=host_name).update(minion_id=host_name, minion_version=version, minion_status='Up')
     for host_name in status_all['down']:
-        #version_dic = {'version': ""}
-        #group_dic = group.hosts_in_group(host_name)
-        #status_dic = {'host': host_name}
-        #status_dic.update(group_dic)
-        #status_dic.update(version_dic)
-        #status_down += [status_dic]
-        #minion_id = Minions_status.objects.get(minion_id=host_name)
-        #minion_id.delete()
         try:
-            minion_id = Minions_status.objects.get(minion_id=host_name)
+            Minions_status.objects.get(minion_id=host_name)
         except:
             status.minion_id = host_name
             status.minion_version = version
@@ -97,3 +98,18 @@ def minions_status_scheduled_job():
 
 minions_status = minions_status_scheduled_job()
 
+
+def grains_scheduled_job():
+    salt_grains = Salt_grains()
+    status = Minions_status.objects.filter(minion_status='Up')
+    for host_name in status:
+        grains = sapi.remote_noarg_execution(host_name.minion_id, 'grains.items')
+        try:
+            Salt_grains.objects.get(minion_id=host_name.minion_id)
+        except:
+            salt_grains.grains = grains
+            salt_grains.minion_id = host_name.minion_id
+            salt_grains.save()
+        Salt_grains.objects.filter(minion_id=host_name.minion_id).update(grains=grains, minion_id=host_name.minion_id)
+
+salt_grains = grains_scheduled_job()
