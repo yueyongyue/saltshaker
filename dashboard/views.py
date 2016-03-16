5738e286387223a7c8d26e07b15e777884b0220e
yueyongyue <yueyognyue@sina.cn>
2016-03-16 17:14:45 +0800

    添加连接不上rabbitmq后的异常处理，添加dashboard页面服务组件状态监控

--- dashboard/views.py
+++ dashboard/views.py
@@ -3,15 +3,18 @@ from django.contrib.auth.decorators import login_required
 from dashboard.models import *
 from returner.models import *
 import logging
-from shaker.tasks import dashboard_task,grains_task
-
+from shaker.tasks import dashboard_task, grains_task
+from shaker.check_service import CheckPort, CheckProgress
 
 logger = logging.getLogger('django')
 
 @login_required(login_url="/account/login/")
 def index(request):
-    dashboard_task.delay()
-    grains_task.delay()
+    try:
+        dashboard_task.delay()
+        grains_task.delay()
+    except:
+        logger.error("Connection refused, don't connect rabbitmq service")
     try:
         dashboard_status = Dashboard_status.objects.get(id=1)
     except:
@@ -41,7 +44,17 @@ def index(request):
         os_all.append(os_dic)
         logger.info(os_all)
 
+    salt_master_stauts = CheckPort('Salt Master', '127.0.0.1', 4505)
+    salt_api_status = CheckPort('Salt Api', '127.0.0.1', 8000)
+    rabbitmy_status = CheckPort('RabbixMQ', '127.0.0.1', 5672)
+    rabbitmy_m_status = CheckPort('RabbixMQ Management', '127.0.0.1', 15672)
+    celery_statu = CheckProgress('Celery', 'celery worker')
+    check_service = [salt_master_stauts, salt_api_status, rabbitmy_status, rabbitmy_m_status, celery_statu]
+
+
+
     return render(request, 'dashboard/index.html', {'status': status_list,
                                                     'os_release': os_release,
                                                     'os_all': os_all,
+                                                    'check_service': check_service,
                                                     })
