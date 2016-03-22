19a181719ec5c1308f867954a739d883c2c2dab9
yueyongyue <yueyognyue@sina.cn>
2016-03-22 17:27:57 +0800

    添加sysconfig_setup 列表

--- system_setup/views.py
+++ system_setup/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
+from shaker.shaker_core import *
+from shaker.nodegroups import *
+from shaker.highstate import *
+import os
+import time
 
-# Create your views here.
+
+
+def system_tools(request):
+    return render(request, 'system_setup/system_tools.html')
