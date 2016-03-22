33b685a31262e0a2a76a500a7d6ae3b15b37c7a9
yueyongyue <yueyognyue@sina.cn>
2016-03-22 13:25:04 +0800

    添加minion_config 字段

--- minions/models.py
+++ minions/models.py
@@ -9,6 +9,7 @@ class Minions_status(models.Model):
     minion_id = models.CharField(max_length=128, null=True, blank=True)
     minion_version = models.CharField(max_length=128, null=True, blank=True)
     minion_status = models.CharField(max_length=128, null=True, blank=True)
+    minion_config = models.CharField(max_length=128, default='unconfig')
 
     def __unicode__(self):
         return u'%s %s %s' % (self.minion_id, self.minion_version, self.minion_status)
