5a83f38f0f40d3be9148ee848117d0af3fe0112d
yueyongyue <yueyognyue@sina.cn>
2016-03-22 13:35:35 +0800

    修改config字段类型为布尔类型

--- minions/models.py
+++ minions/models.py
@@ -9,7 +9,8 @@ class Minions_status(models.Model):
     minion_id = models.CharField(max_length=128, null=True, blank=True)
     minion_version = models.CharField(max_length=128, null=True, blank=True)
     minion_status = models.CharField(max_length=128, null=True, blank=True)
-    minion_config = models.CharField(max_length=128, default='unconfig')
+    minion_config = models.BooleanField(default=False)
+
 
     def __unicode__(self):
         return u'%s %s %s' % (self.minion_id, self.minion_version, self.minion_status)
