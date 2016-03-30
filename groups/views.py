fffd1af852c004c5f50b1604181abbc1934f26c1
root <root@localhost.localdomain>
2016-03-30 15:08:30 +0800

    modify business & privilege module

--- groups/views.py
+++ groups/views.py
@@ -231,7 +231,7 @@ def manage_business(request,*args,**kw):
     _success = kw.get("success",False)
     _error = kw.get("error",False)
     context={
-        "hosts":_businesses,   
+        "businesses":_businesses,   
         "owners":_owners,
         "success":_success,
         "error":_error,
@@ -245,29 +245,23 @@ def del_business(request):
     _error=False
     _ids=request.POST.getlist("id")
     try:
-        _filter=Hosts.objects.filter(id__in=_ids)
+        _filter=Businesses.objects.filter(id__in=_ids)
         _filter.delete()
         _success="Delete opearation successed!"
     except Exception as e:
         _error="Delete opearation error!"
         ErrorLogger.error(str(e))
             
-    return manage_host(request,success=_success,error=_error)
+    return manage_business(request,success=_success,error=_error)
 @login_required(login_url="/account/login/")
 def modify_business(request):
     _success=False
     _error=False
     if request.method=="POST":
         _id=request.POST.get("id")
-        _m=request.POST.get("minion")
-        _minion=Minions_status.objects.get(minion_id=_m)
-        _g=request.POST.get("group")
-        _group=Groups.objects.get(name=_g)
         _name=request.POST.get("name")
-        _business=request.POST.get("business")
         _enabled=request.POST.get("enabled")
         _informations=request.POST.get("informations")
-        _privileges=request.POST.get("privileges")
         _o=request.POST.get("owner")
         _owner=User.objects.get(username=_o)
         if _enabled is not None:
@@ -275,61 +269,46 @@ def modify_business(request):
         else:
             _enabled=False
         try:
-            _host=Hosts.objects.get(id=_id)
-            _name_before=_host.name
-            _host.name=_name
-            _host.minion=_minion
-            _host.group=_group
-            _host.business=_business
-            _host.enabled=_enabled
-            _host.informations=_informations
-            _host.privileges=_privileges
-            _host.owner=_owner
-            _host.save()
-            _success="Modify Group "+ _name +" OK"
+            _business=Businesses.objects.get(id=_id)
+            _name_before=_business.name
+            _business.name=_name
+            _business.enabled=_enabled
+            _business.informations=_informations
+            _business.owner=_owner
+            _business.save()
+            _success="Modify Business "+ _name +" OK"
         except Exception as e:
-            _error="Modify Group "+ _name +" failed"
+            _error="Modify Business "+ _name +" failed"
             ErrorLogger.error(str(e))
-            
         
-    return manage_host(request,success=_success,error=_error)
+    return manage_business(request,success=_success,error=_error)
 @login_required(login_url="/account/login/")
 def add_business(request):
     _success=False
     _error=False
     if request.method=="POST":
-        _m=request.POST.get("minion")
-        _minion=Minions_status.objects.get(minion_id=_m)
-        _g=request.POST.get("group")
-        _group=Groups.objects.get(name=_g)
         _name=request.POST.get("name")
-        _privileges=request.POST.get("privileges")
-        #_current_user=request.user
         _o=request.POST.get("owner")
         _owner=User.objects.get(username=_o)
-        _business=request.POST.get("business")
         _informations=request.POST.get("informations")
-        print _minion,_group
         if request.POST.get("enabled") == "true":
             _enabled=True
         else:
             _enabled=False
         try:
-            _host=Hosts(minion=_minion,group=_group,name=_name,privileges=_privileges,owner=_owner,business=_business,informations=_informations,enabled=_enabled)
-            _host.save()
-            _minion_status=Minions_status.objects.get(minion_id=_m)
-            _minion_status.minion_config=True
-            _minion_status.save()
-            _success="Add Host "+_name+" OK!!"
+            _business=Businesses(name=_name,owner=_owner,informations=_informations,enabled=_enabled)
+            _business.save()
+            _success="Add business line "+_name+" OK!!"
         except Exception as e:
             _error="name already exists or too long!"
             ErrorLogger.error(str(e))
             
     else:
         pass
-    return manage_host(request,success=_success,error=_error)
-###########################  end manage business ###########################
+    return manage_business(request,success=_success,error=_error)
+###########################  end mange business #######################   
 ###########################  mange privilege #######################   
+
 @login_required(login_url="/account/login/")
 def manage_privilege(request,*args,**kw):
     _privileges = Privileges.objects.all()
@@ -337,7 +316,7 @@ def manage_privilege(request,*args,**kw):
     _success = kw.get("success",False)
     _error = kw.get("error",False)
     context={
-        "hosts":_privileges,   
+        "privileges":_privileges,   
         "owners":_owners,
         "success":_success,
         "error":_error,
@@ -351,29 +330,23 @@ def del_privilege(request):
     _error=False
     _ids=request.POST.getlist("id")
     try:
-        _filter=Hosts.objects.filter(id__in=_ids)
+        _filter=Privileges.objects.filter(id__in=_ids)
         _filter.delete()
         _success="Delete opearation successed!"
     except Exception as e:
         _error="Delete opearation error!"
         ErrorLogger.error(str(e))
             
-    return manage_host(request,success=_success,error=_error)
+    return manage_privilege(request,success=_success,error=_error)
 @login_required(login_url="/account/login/")
 def modify_privilege(request):
     _success=False
     _error=False
     if request.method=="POST":
         _id=request.POST.get("id")
-        _m=request.POST.get("minion")
-        _minion=Minions_status.objects.get(minion_id=_m)
-        _g=request.POST.get("group")
-        _group=Groups.objects.get(name=_g)
         _name=request.POST.get("name")
-        _business=request.POST.get("business")
         _enabled=request.POST.get("enabled")
         _informations=request.POST.get("informations")
-        _privileges=request.POST.get("privileges")
         _o=request.POST.get("owner")
         _owner=User.objects.get(username=_o)
         if _enabled is not None:
@@ -381,57 +354,41 @@ def modify_privilege(request):
         else:
             _enabled=False
         try:
-            _host=Hosts.objects.get(id=_id)
-            _name_before=_host.name
-            _host.name=_name
-            _host.minion=_minion
-            _host.group=_group
-            _host.business=_business
-            _host.enabled=_enabled
-            _host.informations=_informations
-            _host.privileges=_privileges
-            _host.owner=_owner
-            _host.save()
-            _success="Modify Group "+ _name +" OK"
+            _privilege=Privileges.objects.get(id=_id)
+            _name_before=_privilege.name
+            _privilege.name=_name
+            _privilege.enabled=_enabled
+            _privilege.informations=_informations
+            _privilege.owner=_owner
+            _privilege.save()
+            _success="Modify privilege "+ _name +" OK"
         except Exception as e:
-            _error="Modify Group "+ _name +" failed"
+            _error="Modify privilege "+ _name +" failed"
             ErrorLogger.error(str(e))
-            
         
-    return manage_host(request,success=_success,error=_error)
+    return manage_privilege(request,success=_success,error=_error)
 @login_required(login_url="/account/login/")
 def add_privilege(request):
     _success=False
     _error=False
     if request.method=="POST":
-        _m=request.POST.get("minion")
-        _minion=Minions_status.objects.get(minion_id=_m)
-        _g=request.POST.get("group")
-        _group=Groups.objects.get(name=_g)
         _name=request.POST.get("name")
-        _privileges=request.POST.get("privileges")
-        #_current_user=request.user
         _o=request.POST.get("owner")
         _owner=User.objects.get(username=_o)
-        _business=request.POST.get("business")
         _informations=request.POST.get("informations")
-        print _minion,_group
         if request.POST.get("enabled") == "true":
             _enabled=True
         else:
             _enabled=False
         try:
-            _host=Hosts(minion=_minion,group=_group,name=_name,privileges=_privileges,owner=_owner,business=_business,informations=_informations,enabled=_enabled)
-            _host.save()
-            _minion_status=Minions_status.objects.get(minion_id=_m)
-            _minion_status.minion_config=True
-            _minion_status.save()
-            _success="Add Host "+_name+" OK!!"
+            _privilege=Privileges(name=_name,owner=_owner,informations=_informations,enabled=_enabled)
+            _privilege.save()
+            _success="Add privilege "+_name+" OK!!"
         except Exception as e:
             _error="name already exists or too long!"
             ErrorLogger.error(str(e))
             
     else:
         pass
-    return manage_host(request,success=_success,error=_error)
+    return manage_privilege(request,success=_success,error=_error)
 ###########################  end manage privilege ###########################
