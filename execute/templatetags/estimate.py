42e57caff23ac53a7409666320d8c5da4016f0db
yueyongyue <yueyognyue@sina.cn>
2016-06-06 08:50:38 +0800

    添加模板过滤器，排序minion

--- execute/templatetags/estimate.py
+++ execute/templatetags/estimate.py
@@ -17,5 +17,12 @@ def to_str(value):
     else:
         return value
 
+@register.filter(is_safe=True)
+def sort_ip(ip_list):
+    try:
+        return ip_list.sort(lambda x, y: int(x.split('.')[3])-int(y.split('.')[3]))
+    except:
+        return ip_list
+
 
 
